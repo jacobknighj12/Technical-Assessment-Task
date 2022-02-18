5917f2c63194c1413fc982c068ba5371a9bac02b
Jacob K <jacobknighj@gmail.com>
2022-02-18 15:32:01 +1100

    added editabilty and hosting to heroku

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -1,12 +1,17 @@
-import react from 'react';
+import react, { useState } from 'react';
 
 export default function EventPage() {
+    const [edit, setEdit] = useState(false)
+
+    function handleClick() {
+        setEdit(!edit)
+    }
     return (
         <div>
             <img className='cover' src='cover.png'></img>
             <div >
-                <h2 className='event'>&#127802; Anica's 22nd </h2><h2 className='event'>Birthday</h2>
-                <div><p className='event'>Hosted by Anica</p></div>
+                <h2 contentEditable={edit} className='event'>&#127802; Anica's 22nd </h2><h2 className='event'>Birthday</h2>
+                <div><p contentEditable={edit} className='event'>Hosted by Anica</p></div>
             </div>
             <div>
                 <img className='going' src='going.png'></img>
@@ -19,17 +24,18 @@ export default function EventPage() {
             </div>
             <div>
                 <img className='calendar' src='calendar.png'></img>
-                <h4 className='event-content'>Sat 16 Oct 2022 6:00PM</h4>
-                <h4 className='event-content'>to Sat 16 Oct 2022 11:00PM</h4>
+                <h4 contentEditable={edit} className='event-content'>Sat 16 Oct 2022 6:00PM</h4>
+                <h4 contentEditable={edit} className='event-content'>to Sat 16 Oct 2022 11:00PM</h4>
                 <p className='event-content'>Add to calendar</p>
             </div>
             <div>
                 <img className='pin' src='pin.png'></img>
-                <h4 className='event-content'>84 Davey Street</h4>
+                <h4 contentEditable={edit} className='event-content'>84 Davey Street</h4>
                 <p className='event-content'>Open in maps</p>
             </div>
             <img className='details' src='details.png'></img>
             <img className='posts' src='posts.png'></img>
+            <button onClick={handleClick}>edit content</button>
         </div>
     )
 }
